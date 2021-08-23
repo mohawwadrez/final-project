6929ad5d1c6daefea35a483dc658200ba055b36a
mohawwadrez <mohawwadrez@yahoo.com>
2021-08-24 01:44:04 +0430

    new style added

--- src/app/component/Banner.jsx
+++ src/app/component/Banner.jsx
@@ -8,15 +8,15 @@ const Banner = () => {
             <div className="container h-100">
                 <div className="row h-100 justify-content-end">
                     <div className="col-6 align-self-center">
-                        <h4>
+                        <h4 className="textTitle p-2 px-5">
                             شرکت توانیر
                         </h4>
-                        <p>
+                        <p className="texbanner p-2 px-5">
                             در صورت تمایل به ثبت درخواست جدید، به بخش ثبت درخواست و در صورت تمایل به پیگیری درخواست قبلی ، با در دست داشتن کد رهگیری درخواست خود، به بخش پیگیری درخواست مراجعه نمایید.
                         </p>
                         <div className="row p-2 px-5">
                             <Link to="/ticket/add">
-                                <button className="btn btn-outline-danger rounded-pill w-100">
+                                <button className="btn btn-danger rounded-pill w-100 btnbaner">
                                     اعلام خسارت
                                 </button>
                             </Link>
@@ -24,16 +24,19 @@ const Banner = () => {
                         </div>
                         <div className="row p-2 px-5">
                             <Link to="ticket/detail">
-                                <button className="btn btn-outline-danger rounded-pill w-100">
+                                <button className="btn btn-danger rounded-pill w-100 btnbaner">
                                     پیگیری خسارت
                                 </button>
                             </Link>
 
                         </div>
                         <div className="row p-2 px-5">
-                            <button className="btn btn-outline-danger rounded-pill">
+                        <Link to="ticket/detail">
+                        <button className="btn  btn-danger rounded-pill w-100 btnbaner">
                                 راهنمای کاربر
                             </button>
+                            </Link>
+                            
                         </div>
                     </div>
                 </div>
--- src/app/component/Header.jsx
+++ src/app/component/Header.jsx
@@ -1,24 +1,31 @@
 import React from "react"
-
+import "../styles/header.css"
+import img from "../Assets/images/bargh.png"
 const Header = () => {
 
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-secondary sticky-top">
+        <nav className="header navbar navbar-expand-lg navbar-dark sticky-top">
             <div class="container">
-                <a class="navbar-brand">Navbar</a>
+                <a class="logo navbar-brand">
+                    <img className="imglogo" src={img} alt="" />
+                    <span className="texlogo">شرکت برق</span>
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" >صفحه نخست</a>
+                            <a class="nav-link active navlink1" aria-current="page" >
+                                صفحه نخست
+                             
+                                </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link">اعلام خسارت</a>
+                            <a class="nav-link navlink">اعلام خسارت</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" >پیگیری خسارت</a>
+                            <a class="nav-link navlink" >پیگیری خسارت</a>
                         </li>
                     </ul>
                 </div>
